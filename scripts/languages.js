dbe504b9e2de58090f9086bb584833c7001a5ee8
Felipe Goncam <felipemaifredo@gmail.com>
2022-09-20 22:07:08 -0300

    v2

--- scripts/languages.js
+++ scripts/languages.js
@@ -3,14 +3,14 @@ const changeEN = document.querySelector("img.en");
 
 const navItems = document.querySelectorAll("a.menu-a");
 const textsSec1 = document.querySelectorAll("[text-sec1]");
-
+const textsSec2 = document.querySelectorAll("[text-sec2]");
 
 changePT.addEventListener("click", () => {
    changeEN.classList.remove("language-active");
    changePT.classList.add("language-active")
 
    //NAV
-   navItems[0].innerText = "Casa";
+   navItems[0].innerText = "Home";
    navItems[1].innerText = "Sobre";
    navItems[2].innerText = "Habilidades";
    navItems[3].innerText = "Projetos";
@@ -28,7 +28,12 @@ changePT.addEventListener("click", () => {
    textsSec1[4].innerText = "de Felipe Gongam,";
    textsSec1[5].innerText = "Um zé mané qualquer!";
    //FIM SEC 1
-
+   //SEC 2
+   textsSec2[0].innerText = "Eu e apenas eu";
+   textsSec2[1].innerHTML = "Me chamo Felipe Goncalves, tenho 21 anos, sou morador de Brasília, formado como técnico de informática pelo IFB e atualmente estou graduando em Análise e Desenvolvimento de Sistemas.<br><br>Tive meu primeiro contato com programação em 2017 no ensino médio, e de lá pra cá venho me desenvolvendo minha carreira profissional na área de TI.<br><br>Tenho como objetivo me tonar um grande profissional na área de front-end, e pensando nisso criei esse portifólio para demonstrar minhas habilidades através dos meus projetos.<br>";
+   textsSec2[2].innerText = "Objetivo";
+   textsSec2[3].innerHTML = "Me chamo Felipe Goncalves, tenho 21 anos, sou morador de Brasília, formado como técnico de informática pelo IFB e atualmente estou graduando em Análise e Desenvolvimento de Sistemas.<br><br>Tive meu primeiro contato com programação em 2017 no ensino médio, e de lá pra cá venho me desenvolvendo minha carreira profissional na área de TI.<br><br>Tenho como objetivo me tonar um grande profissional na área de front-end, e pensando nisso criei esse portifólio para demonstrar minhas habilidades através dos meus projetos.<br>";
+   //FIM SEC 2
 });
 
 changeEN.addEventListener("click", () => {
@@ -54,5 +59,12 @@ changeEN.addEventListener("click", () => {
    textsSec1[4].innerText = "Of Felipe Gongam,";
    textsSec1[5].innerText = "Any any any any any!";
    //FIM SEC 1
+    //SEC 2
+    textsSec2[0].innerText = "Me and just me";
+    textsSec2[1].innerHTML = "My name is Felipe Goncalves, I'm 21 years old, I'm a resident of Brasília, graduated as a computer technician at the IFB and I'm currently graduating in Systems Analysis and Development.<br><br>I had my first contact with programming in 2017 in high school, and since then I have been developing my professional career in the IT field.<br><br>I aim to become a great professional in the front-end area, and with that in mind I created this portfolio to demonstrate my skills through my projects.";
+    textsSec2[2].innerText = "Objective";
+    textsSec2[3].innerHTML = "My name is Felipe Goncalves, I'm 21 years old, I'm a resident of Brasília, graduated as a computer technician at the IFB and I'm currently graduating in Systems Analysis and Development.<br><br>I had my first contact with programming in 2017 in high school, and since then I have been developing my professional career in the IT field.<br><br>I aim to become a great professional in the front-end area, and with that in mind I created this portfolio to demonstrate my skills through my projects.";
+
+    //FIM SEC 2
 
 });
