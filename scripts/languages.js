a34850012eade0385b7ae140084e0c5745112186
Felipe Goncam <felipemaifredo@gmail.com>
2022-09-21 11:57:01 -0300

    v2

--- scripts/languages.js
+++ scripts/languages.js
@@ -4,6 +4,7 @@ const changeEN = document.querySelector("img.en");
 const navItems = document.querySelectorAll("a.menu-a");
 const textsSec1 = document.querySelectorAll("[text-sec1]");
 const textsSec2 = document.querySelectorAll("[text-sec2]");
+const textsSec3 = document.querySelectorAll("[text-sec3]");
 
 changePT.addEventListener("click", () => {
    changeEN.classList.remove("language-active");
@@ -34,6 +35,12 @@ changePT.addEventListener("click", () => {
    textsSec2[2].innerText = "Objetivo";
    textsSec2[3].innerHTML = "Me chamo Felipe Goncalves, tenho 21 anos, sou morador de Brasília, formado como técnico de informática pelo IFB e atualmente estou graduando em Análise e Desenvolvimento de Sistemas.<br><br>Tive meu primeiro contato com programação em 2017 no ensino médio, e de lá pra cá venho me desenvolvendo minha carreira profissional na área de TI.<br><br>Tenho como objetivo me tonar um grande profissional na área de front-end, e pensando nisso criei esse portifólio para demonstrar minhas habilidades através dos meus projetos.<br>";
    //FIM SEC 2
+   // SEC 3
+   textsSec3[0].innerText = "HTML 5 e CSS 3"
+   textsSec3[1].innerText = "Semântica"
+   textsSec3[2].innerText = "Acessibilidade"
+   textsSec3[3].innerText = "Responsividade"
+   // FIM SEC 3
 });
 
 changeEN.addEventListener("click", () => {
@@ -49,7 +56,7 @@ changeEN.addEventListener("click", () => {
     navItems[5].innerText = "Works";
     navItems[6].innerText = "What if...?";
     navItems[7].innerText = "Blog";
-    navItems[8].innerText = "Talk to Me";
+    navItems[8].innerText = "Contact";
    //FIM NAV
    // SEC 1
    textsSec1[0].innerText = "Hi";
@@ -64,7 +71,12 @@ changeEN.addEventListener("click", () => {
     textsSec2[1].innerHTML = "My name is Felipe Goncalves, I'm 21 years old, I'm a resident of Brasília, graduated as a computer technician at the IFB and I'm currently graduating in Systems Analysis and Development.<br><br>I had my first contact with programming in 2017 in high school, and since then I have been developing my professional career in the IT field.<br><br>I aim to become a great professional in the front-end area, and with that in mind I created this portfolio to demonstrate my skills through my projects.";
     textsSec2[2].innerText = "Objective";
     textsSec2[3].innerHTML = "My name is Felipe Goncalves, I'm 21 years old, I'm a resident of Brasília, graduated as a computer technician at the IFB and I'm currently graduating in Systems Analysis and Development.<br><br>I had my first contact with programming in 2017 in high school, and since then I have been developing my professional career in the IT field.<br><br>I aim to become a great professional in the front-end area, and with that in mind I created this portfolio to demonstrate my skills through my projects.";
-
     //FIM SEC 2
+    // SEC 3
+   textsSec3[0].innerText = "HTML 5 e CSS 3"
+   textsSec3[1].innerText = "Semantics"
+   textsSec3[2].innerText = "Accessibility"
+   textsSec3[3].innerText = "Responsivity"
+   // FIM SEC 3
 
 });
