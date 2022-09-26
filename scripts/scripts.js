299b735e274442e8402de19bb0e7d528cebf592b
Felipe Goncam <felipemaifredo@gmail.com>
2022-09-26 12:01:57 -0300

    v3

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -23,3 +23,21 @@ function auto() {
 }
 auto()
 // FIM CARROSSEL PAGE 1 
+// FORM SEND
+document.querySelector("form").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let name = document.querySelector("input[name=nome]").value;
+    let whatsEmail = document.querySelector("input[name=whatsappemail]").value;
+    let assunto = document.querySelector("input[name=assunto]").value;
+
+    fetch("https://api.sheetmonkey.io/form/7JKr1TpjSCqNPfTEju5jZ8", {
+        method: "post",
+        headers: {
+            "Accept": "aplication/json", 
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ nome: name, WhatsAppEmail: whatsEmail, Assunto: assunto }),
+    });
+}); 
+// FIM FORM SEND
