096d7ebffd2f7a7a45c350e273184e7c8ab63fe8
Felipe Maifredo <felipemaifredo@gmail.com>
2022-11-13 01:35:38 -0300

    v att

--- scripts/languages-tools.js
+++ scripts/languages-tools.js
@@ -32,18 +32,18 @@ function changePTFunc() {
    // GENERATE PALLETE  
         textsTool[0].innerText = "Gerador de Paleta de Cores";
         textsTool[1].innerHTML = `Surgiu como uma evolução do projeto de gerador de cores usando ${spanJS}, ${spanHTML} and ${spanCSS} porém gerando código hexadecimal`;
-        textsTool[2].innerHTML = `${BTNlink} Ver Projeto`;
-        textsTool[3].innerHTML = `${BTNgithub} Ver Github`;
+        textsTool[2].innerHTML = `Ver Projeto`;
+        textsTool[3].innerHTML = `Ver Github`;
     //  CALC
         textsTool[4].innerText = "Calculadora";
         textsTool[5].innerHTML = `Uma evolução da Calculadora Simples usando ${spanJS}, ${spanHTML} e ${spanCSS} porém mais complexa e completa`;
-        textsTool[6].innerHTML = `${BTNlink} Ver Projeto`;
-        textsTool[7].innerHTML = `${BTNgithub} Ver Github`;
+        textsTool[6].innerHTML = `Ver Projeto`;
+        textsTool[7].innerHTML = `Ver Github`;
      //  REGRA DE 3
         textsTool[8].innerText = "Regra de 3";
         textsTool[9].innerHTML = `Criada para treinar a lógica de programação em ${spanJS} e manipulação de DOM ${spanHTML} e ${spanCSS}`;
-        textsTool[10].innerHTML = `${BTNlink} Ver Projeto`;
-        textsTool[11].innerHTML = `${BTNgithub} Ver Github`;
+        textsTool[10].innerHTML = `Ver Projeto`;
+        textsTool[11].innerHTML = `Ver Github`;
 };
 
 function changeENFunc() {
@@ -64,18 +64,18 @@ function changeENFunc() {
    // GENERATE PALLETE
         textsTool[0].innerText = "Color Palette Generator";
         textsTool[1].innerHTML = `It emerged as an evolution of the color generator design using ${spanJS}, ${spanHTML} and ${spanCSS} but generating hex code`;
-        textsTool[2].innerHTML = `${BTNlink} See Project`;
-        textsTool[3].innerHTML = `${BTNgithub} See Github`;
+        textsTool[2].innerHTML = `See Project`;
+        textsTool[3].innerHTML = `See Github`;
     //  CALC
         textsTool[4].innerText = "Calculator";
         textsTool[5].innerHTML = `An evolution of the Simple Calculator using ${spanJS}, ${spanHTML} and ${spanCSS} but more complex and complete`;
-        textsTool[6].innerHTML = `${BTNlink} See Project`;
-        textsTool[7].innerHTML = `${BTNgithub} See Github`;
+        textsTool[6].innerHTML = `See Project`;
+        textsTool[7].innerHTML = `See Github`;
      // REGRA DE 3
         textsTool[8].innerText = "rule of 3";
         textsTool[9].innerHTML = `Created to train programming logic in ${spanJS} and DOM manipulation ${spanHTML} and ${spanCSS}`;
-        textsTool[10].innerHTML = `${BTNlink} See Project`;
-        textsTool[11].innerHTML = `${BTNgithub} See Github`;
+        textsTool[10].innerHTML = `See Project`;
+        textsTool[11].innerHTML = `See Github`;
 };
 
 function detectIdiom() {
